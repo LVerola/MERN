bc1dbffb6174496015bf6d1bbca544809b9ddbe5
Luis Gabriel Verola Santos <62057819+LVerola@users.noreply.github.com>
2020-06-26 11:00:53 -0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const db = require('./db')
 const productRouter = require('./routes/product-router')
 
 const app = express()
-const apiPort = 3000
+const apiPort = 3333
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cors())
